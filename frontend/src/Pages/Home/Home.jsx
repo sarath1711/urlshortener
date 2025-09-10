31e913c194c1e48a4e13ce78a87a8431fb4b8e03
Sarath <sarathbhushan04@gmail.com>
2025-09-10 11:58:45 +0530

    next commit

--- frontend/src/Pages/Home/Home.jsx
+++ frontend/src/Pages/Home/Home.jsx
@@ -53,7 +53,7 @@ const Home = () => {
               marginTop: "1rem",
             }}
           >
-            Your App Name
+            Sarath "URL Shortener"
           </Title>
 
           <Text
